03d79204d72b2b0a480a10fff76534b1e445ae36
[mateusz bereszczuk] <[matebere@gmail.com]>
2018-05-30 17:46:24 +0200

    Add

---
+++ ClassLibrary1/Exercise3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorNS
+{
+    public class Exercise3
+    {
+        public string deleteSymb(string a1, string b1)
+        {
+            bool flag;
+
+            for (int i = 0; i < b1.Length; i++)
+            {
+                do
+                {
+                    flag = a1.Contains(b1[i].ToString());
+                    if (flag == true)
+                    {
+                        int numberOfSymb;
+                        numberOfSymb = a1.IndexOf(b1[i]);
+                        a1 = a1.Remove(numberOfSymb, 1);
+                    }
+                } while (flag == true);
+            }
+            return a1;
+        }
+    }
+}
---
+++ UnitTestProject1/EssentialOperationsTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CalculatorNS;
+using ClassLibrary1;
+
+namespace CalculatorNS.Tests
+{
+    [TestClass()]
+    public class EssentialOperationsTest
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            var Calculator = new Calculator();
+            var result = Calculator.Sum(1, 2);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod()]
+        public void minusTest()
+        {
+            var Calculator = new Calculator();
+            var result = Calculator.Minus(1, 2);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod()]
+        public void methodWihoutFullCoverageTest()
+        {
+            var Calculator = new Calculator();
+            var result = Calculator.methodWithoutFullCoverage(10);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod()]
+        public void trueLogin()
+        {
+            var Exercise1 = new Exercise1();
+            bool result = Exercise1.login("user", "pass");
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod()]
+        public void fail30MUer()
+        {
+            var Exercise1 = new Exercise1();
+            bool result = Exercise1.login("user1111111111111111111111111111111", "pass");
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod()]
+        public void fail30MPass()
+        {
+            var Exercise1 = new Exercise1();
+            bool result = Exercise1.login("user", "pass1111111111111111111111111111111");
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod()]
+        public void blankUser()
+        {
+            var Exercise1 = new Exercise1();
+            bool result = Exercise1.login("", "pass");
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod()]
+        public void blankPass()
+        {
+            var Exercise1 = new Exercise1();
+            bool result = Exercise1.login("user", "");
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod()]
+        public void trueAsteriscs()
+        {
+            var Exercise2 = new Exercise2();
+            string result = Exercise2.asteriscs(3);
+            Assert.AreEqual("***", result);
+        }
+        [TestMethod()]
+        public void falseAsteriscs()
+        {
+            var Exercise2 = new Exercise2();
+            string result = Exercise2.asteriscs(-3);
+            Assert.AreEqual("incorrect number", result);
+        }
+        [TestMethod()]
+        public void truedeleteSymb()
+        {
+            var Exercise3 = new Exercise3();
+            string result = Exercise3.deleteSymb("ba","ac");
+            Assert.AreEqual("b", result);
+        }
+        [TestMethod()]
+        public void truemultideleteSymb()
+        {
+            var Exercise3 = new Exercise3();
+            string result = Exercise3.deleteSymb("qadeee", "vce");
+            Assert.AreEqual("qad", result);
+        }
+    }
+}
+
+
